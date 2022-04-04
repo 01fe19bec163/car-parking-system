30dc5638754a4e458d030e28cf734c92c7c758ce
Abdul Ahad <59968630+01fe19bec163@users.noreply.github.com>
2022-04-04 22:18:59 +0530

    Update parking_system.v

--- parking_system.v
+++ parking_system.v
@@ -19,18 +19,17 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module parking_system( 
-                input clk,reset_n,
- input sensor_entrance, sensor_exit, 
+ input clk,reset_n,sensor_entrance, sensor_exit, 
  input [1:0] password_1, password_2,
  output wire GREEN_LED,RED_LED,
  output reg [6:0] HEX_1, HEX_2
     );
  parameter IDLE = 3'b000, WAIT_PASSWORD = 3'b001, WRONG_PASS = 3'b010, RIGHT_PASS = 3'b011,STOP = 3'b100;
- // Moore FSM : output just depends on the current state
+ 
  reg[2:0] current_state, next_state;
  reg[31:0] counter_wait;
  reg red_tmp,green_tmp;
- // Next state
+ 
  always @(posedge clk or negedge reset_n)
  begin
  if(~reset_n) 
@@ -38,7 +37,7 @@ module parking_system(
  else
  current_state = next_state;
  end
- // counter_wait
+ 
  always @(posedge clk or negedge reset_n) 
  begin
  if(~reset_n) 
@@ -48,8 +47,7 @@ module parking_system(
  else 
  counter_wait <= 0;
  end
- // change state
-// fpga4student.com FPGA projects, Verilog projects, VHDL projects
+
  always @(*)
  begin
  case(current_state)
@@ -93,38 +91,38 @@ module parking_system(
  default: next_state = IDLE;
  endcase
  end
- // LEDs and output, change the period of blinking LEDs here
+ 
  always @(posedge clk) begin 
  case(current_state)
  IDLE: begin
  green_tmp = 1'b0;
  red_tmp = 1'b0;
- HEX_1 = 7'b1111111; // off
- HEX_2 = 7'b1111111; // off
+ HEX_1 = 7'b1111111; 
+ HEX_2 = 7'b1111111; 
  end
  WAIT_PASSWORD: begin
  green_tmp = 1'b0;
  red_tmp = 1'b1;
- HEX_1 = 7'b000_0110; // E
- HEX_2 = 7'b010_1011; // n 
+ HEX_1 = 7'b000_0110; 
+ HEX_2 = 7'b010_1011; 
  end
  WRONG_PASS: begin
  green_tmp = 1'b0;
  red_tmp = ~red_tmp;
- HEX_1 = 7'b000_0110; // E
- HEX_2 = 7'b000_0110; // E 
+ HEX_1 = 7'b000_0110;
+ HEX_2 = 7'b000_0110;
  end
  RIGHT_PASS: begin
  green_tmp = ~green_tmp;
  red_tmp = 1'b0;
- HEX_1 = 7'b000_0010; // 6
- HEX_2 = 7'b100_0000; // 0 
+ HEX_1 = 7'b000_0010; 
+ HEX_2 = 7'b100_0000; 
  end
  STOP: begin
  green_tmp = 1'b0;
  red_tmp = ~red_tmp;
- HEX_1 = 7'b001_0010; // 5
- HEX_2 = 7'b000_1100; // P 
+ HEX_1 = 7'b001_0010; 
+ HEX_2 = 7'b000_1100; 
  end
  endcase
  end
